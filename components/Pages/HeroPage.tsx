00654bbd81dab2cee640a433083d6d114fe26a00
Amitoj <amitoj.mehta@gmail.com>
2025-08-30 08:01:45 +0530

    hero responsive

--- components/Pages/HeroPage.tsx
+++ components/Pages/HeroPage.tsx
@@ -7,16 +7,16 @@ import HeroImage from "../HeroImage";
 
 const HeroPage = () => {
   return (
-    <section className="mt-18 h-[calc(100vh-4.5rem)]">
+    <section className="mt-18 h-[70vh] md:h-[calc(100vh-4.5rem)]">
       <div className="container mx-auto py-4 px-4 xl:w-7xl h-full">
         <div className="flex justify-center md:justify-between items-center h-full">
           <div className="flex flex-col gap-6 items-center md:items-start text-center md:text-left">
-            <div className="text-4xl font-semibold">
+            <div className="text-2xl md:text-4xl font-semibold">
               <span>Welcome to</span>
-              <WebTitle className="text-8xl" />
+              <WebTitle className="text-7xl md:text-8xl" />
             </div>
 
-            <p className="text-lg text-muted-foreground font-mono max-w-xl">
+            <p className="text-md md:text-xl text-muted-foreground font-mono max-w-xl">
               Protect your home with smart RFID-based access control. Manage
               members, track entry logs, and stay secure — all in one place.
             </p>
